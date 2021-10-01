7d2e9cf353f30c8a585e05223f544804930fa76f
TheObands1 <juansebastian3080@hotmail.com>
2021-10-01 10:47:01 -0500

    Created Dashing Ability

--- Assets/Scripts/MegamanPlayer.cs
+++ Assets/Scripts/MegamanPlayer.cs
@@ -6,14 +6,19 @@ public class MegamanPlayer : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float jumpSpeed;
+    [SerializeField] float dashSpeed;
     [SerializeField] BoxCollider2D FeetCollider;
     [SerializeField] Sprite IdleSprite;
     [SerializeField] Sprite FallingSprite;
-
+    [SerializeField] float dashTime;
+    [SerializeField] float StartDashTime;
+    
     Animator myAnimator;
     SpriteRenderer myRenderer;
     Rigidbody2D myRigidBody2D;
     BoxCollider2D myMainCollider;
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +27,8 @@ public class MegamanPlayer : MonoBehaviour
         myRenderer = GetComponent<SpriteRenderer>();
         myRigidBody2D = GetComponent<Rigidbody2D>();
         myMainCollider = GetComponent<BoxCollider2D>();
+
+        dashTime = StartDashTime;
     }
 
     // Update is called once per frame
@@ -31,17 +38,18 @@ public class MegamanPlayer : MonoBehaviour
        Jump();
        CharacterFallingDetector();
        Fire();
+       Dash();
     }
 
     void Movement()
     {
         float CurrentMovement = Input.GetAxis("Horizontal");
-
         if (CurrentMovement != 0)
         {
             myAnimator.SetBool("isRunning", true);
             transform.localScale = new Vector2(Mathf.Sign(CurrentMovement), 1);
             transform.Translate(new Vector2(CurrentMovement * speed * Time.deltaTime, 0));
+          
         }
         else
         {
@@ -58,9 +66,15 @@ public class MegamanPlayer : MonoBehaviour
             myAnimator.SetBool("IsJumping", false);
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                float extraJumpSpeedForDashing = 1.0f;
+                if(myAnimator.GetBool("IsDashing"))
+                {
+                    extraJumpSpeedForDashing = 20.0f;
+                }
                 myAnimator.SetTrigger("JumpStartTrigger");
                 myAnimator.SetBool("IsJumping", true);
-                myRigidBody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+                myRigidBody2D.AddForce(new Vector2(0, jumpSpeed*extraJumpSpeedForDashing), ForceMode2D.Impulse);
+                
             }
         }
         else
@@ -72,12 +86,14 @@ public class MegamanPlayer : MonoBehaviour
 
     bool IsTouchingTheGround()
     {
-        //return FeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        /*return FeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));*/
 
-        RaycastHit2D GroundRaycast = Physics2D.Raycast(myMainCollider.bounds.center, Vector2.down, myMainCollider.bounds.extents.y + 0.2f, LayerMask.GetMask("Ground"));
-        Debug.DrawRay(myMainCollider.bounds.center, new Vector2(0, (myMainCollider.bounds.extents.y + 0.2f) * -1), Color.red);
+       
+        RaycastHit2D GroundRaycast = Physics2D.Raycast(myMainCollider.bounds.center, Vector2.down, myMainCollider.bounds.extents.y + 0.35f, LayerMask.GetMask("Ground"));
+        Debug.DrawRay(myMainCollider.bounds.center, new Vector2(0, (myMainCollider.bounds.extents.y + 0.35f) * -1), Color.red);
 
         return GroundRaycast.collider != null;
+        
 
     }
 
@@ -89,7 +105,7 @@ public class MegamanPlayer : MonoBehaviour
 
     void CharacterFallingDetector()
     {
-        if(myRigidBody2D.velocity.y < 0 && !myAnimator.GetBool("IsJumping"))
+        if(myRigidBody2D.velocity.y < 0 && !myAnimator.GetBool("IsJumping") && !myAnimator.GetBool("IsDashing"))
         {
             myAnimator.SetBool("isFalling", true);
         }
@@ -104,6 +120,44 @@ public class MegamanPlayer : MonoBehaviour
         else if(Input.GetKeyUp(KeyCode.X))
         {
             myAnimator.SetLayerWeight(1, 0);
+        }
+    }
+
+    void Dash()
+    {
+        if(!myAnimator.GetBool("IsJumping") && !myAnimator.GetBool("isFalling") && !myAnimator.GetBool("IsDashing"))
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                if(dashTime <= 0)
+                {
+                    dashTime = StartDashTime;
+                    myRigidBody2D.velocity = Vector2.zero;
+                    myAnimator.SetBool("IsDashing", false);
+                }
+                else
+                {
+                    dashTime -= Time.deltaTime;
+                    myAnimator.SetBool("IsDashing", true);
+                    if (transform.localScale.x == 1)
+                    {
+                        myRigidBody2D.velocity = Vector2.right * dashSpeed;
+                    }
+                    else if(transform.localScale.x == -1)
+                    {
+                        myRigidBody2D.velocity = Vector2.left * dashSpeed;
+                    }
+                }
+                /*
+                lastTimePressed = Time.time;
+                myAnimator.SetBool("IsDashing", true);
+                myRigidBody2D.AddForce(new Vector2(dashSpeed * transform.localScale.x, 0), ForceMode2D.Impulse);
+                */
+            }
+        }
+        else
+        {
+            myAnimator.SetBool("IsDashing", false);
         }
     }
 }
